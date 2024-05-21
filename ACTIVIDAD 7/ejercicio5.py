a96d1481b968f1cb261b85c210903707bf53a702
limAle <118579154+alexandralima1602@users.noreply.github.com>
2024-05-21 11:05:39 -0500

    Update ejercicio5.py

--- ACTIVIDAD 7/ejercicio5.py
+++ ACTIVIDAD 7/ejercicio5.py
@@ -1,1 +1,21 @@
+import asyncio
+import aiohttp
+
+#define la funcion asincronica para hacer solicitudes HTTP
+async def fetch(url, session):
+    async with session.get(url) as response:
+        print(f"Status: {response.status}")
+        data = await response.text()
+        print(f"Data from {url} fetched")
+        return data
+#Crea una tarea asincrónica principal que maneje múltiples solicitudes: 
+async def main(urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch(url, session) for url in urls]
+        await asyncio.gather(*tasks)
+#Ejecutar el programa asincrónico: 
+urls = ["https://www.amazon.com"] * 5 # Lista de URLs a consultar
+
+asyncio.run(main(urls))
+
 
